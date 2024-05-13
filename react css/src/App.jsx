059b328ddda2a1e8799ff4bd1f5d51e8b7c778ae
fatimabyaramli <fatima17bayramli@gmail.com>
2024-05-13 11:01:39 +0400

    css react

---
+++ react css/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Header from './assets/components/Header'
+import Main from './assets/components/Main'
+import RecipeReviewCard from './assets/components/Header/index.jsx'
+function App() {
+
+  return (
+    <>
+    <RecipeReviewCard/>
+  
+      {/* <Header/> */}
+      <Main/>
+    </>
+  )
+}
+
+export default App
